35d6c085699beadba405254d63f2b9bd64c8930a
Joaquin Robador <joaquinrobador@gmail.com>
2025-10-16 22:55:27 -0300

    Update Eigenvalues.py

--- Eigenvalues.py
+++ Eigenvalues.py
@@ -9,14 +9,11 @@ Write a Python function that calculates the eigenvalues of a 2x2 matrix. The fun
 from math import sqrt, pow
 
 def calculate_eigenvalues(matrix: list[list[float|int]]) -> list[float]:
-    a = matrix [0][0]
-    b = matrix [0][1]
-    c = matrix [1][0]
-    d = matrix [1][1]
+    a, b = matrix[0]
+    c, d = matrix[1]
 
+    # Characteristic polynomial: λ² - (a + d)λ + (ad - bc) = 0
     eig_1 = ((a+d) + sqrt(pow(a,2)+(2*a*d)+pow(d,2)-(4*a*d)+(4*b*c))) / 2
     eig_2 = ((a+d) - sqrt(pow(a,2)+(2*a*d)+pow(d,2)-(4*a*d)+(4*b*c))) / 2
-
-    eigenvalues = [eig_1, eig_2]
     
-    return eigenvalues
+    return sorted([eig_1, eig_2], reverse=True)
