95f86a2ed7a8fd5c2c0dbcaa2b95c2c4fdaabf4a
Joaquin Robador <joaquinrobador@gmail.com>
2025-10-16 22:07:43 -0300

    Update Scalar-Mult-Matrix.py

--- Scalar-Mult-Matrix.py
+++ Scalar-Mult-Matrix.py
@@ -1,3 +1,11 @@
+"""
+Scalar Multiplication of a Matrix
+Easy
+Linear Algebra
+
+
+Write a Python function that multiplies a matrix by a scalar and returns the result."""
+
 def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
     result = [[el * scalar for el in ar] for ar in matrix]
     return result
